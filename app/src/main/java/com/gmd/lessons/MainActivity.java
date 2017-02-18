49fe2b0e2f01353c6e7d73fb0abfeff628fcecb7
ceciramirezg <ceci.e.ramirezg@gmail.com>
2017-02-17 22:23:46 -0500

    change main activity with a comment 2.

--- app/src/main/java/com/gmd/lessons/MainActivity.java
+++ app/src/main/java/com/gmd/lessons/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //comentario test
+        //comentario test 2
     }
 }
